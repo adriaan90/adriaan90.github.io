47f72a60cb73b650c98b8117376692068d9a5fc0
Adriaan Van Niekerk <asvn90@gmail.com>
2024-07-25 22:22:33 +0200

    Update config page

--- conf.py
+++ conf.py
@@ -6,10 +6,23 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'adriaansblog'
+# -- ABlog ---------------------------------------------------
+
+blog_baseurl = "https://adriaan90.github.io"
+blog_title = "Adriaan's Blog"
+blog_path = "blog"
+blog_post_pattern = "blog/*/*"
+blog_feed_fulltext = True
+blog_feed_subtitle = "Open communities, open science, communication, and data."
+fontawesome_included = True
+post_redirect_refresh = 1
+post_auto_image = 1
+post_auto_excerpt = 2
+
+project = "Adriaan's Blog"
 copyright = '2024, Adriaan van Niekerk'
 author = 'Adriaan van Niekerk'
-release = '1.0'
+release = ""
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -29,6 +42,16 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "pydata_sphinx_theme"
+
+html_theme_options = {
+    "logo": {
+        # In a left-to-right context, screen readers will read the alt text
+        # first, then the text, so this example will be read as "P-G-G-P-Y
+        # (short pause) Home A pretty good geometry package"
+        "text": "Adriaan's Blog"
+    }
+}
+
 html_static_path = ['_static']
 
 def setup(app):
